baeeef6ea3690cc6ccfd83baa5eb100e85cecca9
GoMenXGames <46220650+GoMenXGames@users.noreply.github.com>
2024-05-20 00:57:41 +0500

    rofls

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
+let globalWrap = false;
 
 function ready() {
-  document.querySelector("#inputText").addEventListener("input", inputEdit)
+  document.querySelector("#inputText").addEventListener("input", inputEdit);
+  document.querySelector("input#globalWrap").addEventListener("change", inputEdit);
 }
+
+
 
 function inputEdit (e) {
   // console.log(e.data);
-  text = document.querySelector("#inputText").value
-  document.querySelector("#output").innerHTML = tekken8parse(text)
+  globalWrap = document.querySelector("input#globalWrap").checked
+  text = document.querySelector("#inputText").value;
+  document.querySelector("#output").innerHTML = tekken8parse(text);
 }
 
 function tekken8parse(text){
   text_inputs = text.split(" ");
-  text_inputs.length;
-  arrayImages = ""
+  // text_inputs.length;
+  output = "" //Output
+  textStarted = false;
+  textArray = "";
   text_inputs.forEach(input => {
-    if (input != "") {
-      arrayImages+=(textToImage(input))
+    if (input == "("){
+      textStarted = true
     }
+    else if (input == ")") {
+      textStarted = false
+    }
+    else if ( globalWrap &&  input == ",") {
+      output+= (textToImage(input)) + "<br/>" +(textToImage(input))
+    }
+    else if (input != "" && !textStarted) {
+      output+=(textToImage(input))
+    }
+    else if (textStarted){
+      output += "<div class='text'> "+input+" </div>"
+    }
+    
+
   });
-  return arrayImages
+  return output
 }
 
-function assign(key){
-  switch (key) {
-    case 'h':
-      return "1";
-      break;
-    case 'u':
-      return "2";
-      break;
-  
-    default:
-      return false;
-      break;
-  }
-}
+
 
 function textToImage(text) {
   return "<img src=\"img/"+text+".png\">"
